67313f0a84586d78ccedb766b6a80ca3e1f34cd8
Lee Stafford <lee.staff@gmail.com>
2017-04-24 12:23:29 -0400

    Add files via upload

---
+++ application/views/Admin/GenerateModelClassesSubmit.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Created on Apr 4, 2014
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+?>
+
+<h3>Code Generated for Table: <?php echo $tableName; ?></h3>
+<?php $HTML::ActionLink("Back to Table Selection", "GenerateModelClasses", "Admin"); ?>
+<textarea rows="75" style="margin-left : 50px; width : 90%;" onfocus="select()"><?php echo $code; ?></textarea>
+
